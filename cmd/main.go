aa2557c935ce44484bd0d66a716d0bc7e701fa68
Oppodelldog <odog@netcologne.de>
2021-05-01 13:21:50 +0200

    simplify ide contributions

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	config2 "github.com/Oppodelldog/roamer/internal/config"
-	server2 "github.com/Oppodelldog/roamer/internal/server"
+	"github.com/Oppodelldog/roamer/internal/config"
+	"github.com/Oppodelldog/roamer/internal/server"
 )
 
 func main() {
-	if err := config2.Load(); err != nil {
+	if err := config.Load(); err != nil {
 		panic(err)
 	}
 
-	server2.Start()
+	server.Start()
 }
